010cf56a63bbe79efa8cdd417da38d1db18bc85b
Andcool Systems <56398220+andcool-2007@users.noreply.github.com>
2023-03-16 19:48:19 +0300

    Update socialc.py

--- socialc.py
+++ socialc.py
@@ -1,117 +1,125 @@
 import numpy as np
 import os
-
-def open_file(path):
-	if os.path.exists(f"SocialScore{path}.txt") == False:
-		sc = np.zeros((100, 2))
-		for scc_f in range(99):
-			sc[scc_f][1] = 500
-	else:
-		sc_file = open(f"SocialScore{path}.txt", 'r')
-		i = 0
-		sc = np.zeros((100, 2))
-		for line in sc_file:
-			sc[i] = line.split("/")
-			i += 1
-
-		sc_file.close()
-
-	return sc
-
-def save_file(path, data):
-
-	sc_file = open(f"SocialScore{path}.txt", 'w')
-	i = 0
-	for line in range(100):
-		sc_file.write(f"{data[i][0]}/{data[i][1]}\n")
-		i += 1
-
-	sc_file.close()
-
-
-
-
 def SocialScore(id1, am, chat_id):
-	sc = open_file(chat_id)
+	if os.path.exists("SocialScore" + str(chat_id) + ".npy") == False:
+		sc = np.zeros((3, 100))
+		for sc_c_f in range(99):
+			sc[1][sc_c_f] = 500
 
+	else:
+		sc = np.load("SocialScore" + str(chat_id) + ".npy")
 	finded = False
 	for sc_c in range(99):
-		if sc[sc_c][0] == id1:
-			sc[sc_c][1] += am
+		if sc[0][sc_c] == id1:
+			sc[1][sc_c] += am
 			finded = True
 
 
 	if finded == False:
 		for sc_c_n in range(99):
-			if sc[sc_c_n][0] == 0:
-				sc[sc_c_n][0] = id1
-				sc[sc_c_n][1] += am
+			if sc[0][sc_c_n] == 0:
+				sc[0][sc_c_n] = id1
+				sc[1][sc_c_n] += am
 
 
 				break
-	save_file(chat_id, sc)
+	np.save("SocialScore" + str(chat_id) + ".npy", sc)
 
 def SocialScore_set(id1, am, chat_id):
-	sc = open_file(chat_id)
+	if os.path.exists("SocialScore" + str(chat_id) + ".npy") == False:
+		sc = np.zeros((3, 100))
+		for sc_c_f in range(99):
+			sc[1][sc_c_f] = 500
 
+	else:
+		sc = np.load("SocialScore" + str(chat_id) + ".npy")
 	finded = False
 	for sc_c in range(99):
-		if sc[sc_c][0] == id1:
-			sc[sc_c][1] = am
+		if sc[0][sc_c] == id1:
+			sc[1][sc_c] = am
 			finded = True
 
 
 	if finded == False:
 		for sc_c_n in range(99):
-			if sc[sc_c_n][0] == 0:
-				sc[sc_c_n][0] = id1
-				sc[sc_c_n][1] = am
+			if sc[0][sc_c_n] == 0:
+				sc[0][sc_c_n] = id1
+				sc[1][sc_c_n] = am
 
 
 				break
-	save_file(chat_id, sc)
+	np.save("SocialScore" + str(chat_id) + ".npy", sc)
 
 def SocialScore_setp(id1, am, chat_id):
-	sc = open_file(chat_id)
+	if os.path.exists("SocialScore" + str(chat_id) + ".npy") == False:
+		sc = np.zeros((3, 100))
+		for sc_c_f in range(99):
+			sc[1][sc_c_f] = 500
 
+	else:
+		sc = np.load("SocialScore" + str(chat_id) + ".npy")
 	finded = False
 	for sc_c in range(99):
-		if sc[sc_c][0] == id1:
-			sc[sc_c][1] = am
+		if sc[0][sc_c] == id1:
+			sc[1][sc_c] = am
 			finded = True
 
 
 	if finded == False:
 		for sc_c_n in range(99):
-			if sc[sc_c_n][0] == 0:
-				sc[sc_c_n][0] = id1
-				sc[sc_c_n][1] = am
+			if sc[0][sc_c_n] == 0:
+				sc[0][sc_c_n] = id1
+				sc[1][sc_c_n] = am
 
 
 				break
-	save_file(chat_id, sc)
+	np.save("SocialScore" + str(chat_id) + ".npy", sc)
+
+
+def SocialScore_erase(id1, chat_id):
+	if os.path.exists("SocialScore" + str(chat_id) + ".npy") == False:
+		sc = np.zeros((3, 100))
+		for sc_c_f in range(99):
+			sc[1][sc_c_f] = 500
+
+	else:
+		sc = np.load("SocialScore" + str(chat_id) + ".npy")
+	
+	for sc_c in range(100):
+		if sc[0][sc_c] == id1:
+			sc[0][sc_c] = 0
+			sc[1][sc_c] = 500
+			sc[2][sc_c] = 0
 
 
 
+
+
+
+
+
+	np.save("SocialScore" + str(chat_id) + ".npy", sc)
 
 
 def show(id1, chat_id):
-	sc = open_file(chat_id)
+	if os.path.exists("SocialScore" + str(chat_id) + ".npy") == False:
+		sc = np.zeros((3, 100))
+		for sc_c_f in range(99):
+			sc[1][sc_c_f] = 500
 
+	else:
+		sc = np.load("SocialScore" + str(chat_id) + ".npy")
 	finded = False
 	sc_am = 0
 	for sc_c in range(99):
-		if sc[sc_c][0] == id1:
+		if sc[0][sc_c] == id1:
 			finded = True
-			sc_am = sc[sc_c][1]
+			sc_am = sc[1][sc_c]
 
 	if finded == False:
 		for sc_c_n in range(99):
-			if sc[sc_c_n][0] == 0:
-				sc[sc_c_n][0] = id1
-				sc_am = sc[sc_c_n][1]
+			if sc[0][sc_c_n] == 0:
+				sc[0][sc_c_n] = id1
+				sc_am = sc[1][sc_c_n]
 				break
 	return sc_am
-
-
-
